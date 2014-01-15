5af616c9de22a5022d8129f3a2a010df55c8820b
Ryan Kois <ryan.kois@gmail.com>
2014-01-14 23:05:47 -0700

    Dat firsties.

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var cool = require('cool-ascii-faces');
+var faces = require('cool-ascii-faces').faces;
+
+app.get('/', function(req, res) {
+  face = cool();
+  res.send(cool());
+});
+
+app.get('/api/v1/faces', function(req, res) {
+  var payload = {
+    "faces": faces.map(function(face, index, arr) {
+      return { 'id': index, 'face': face };
+    })
+  };
+
+  res.send(payload);
+});
+
+app.get('/api/v1/faces/:id', function(req, res) {
+  if (req.params.id > faces.length) res.send(404, 'Woah, like totally no faces here!'); 
+
+  var payload = {"faces": [{
+    "id": req.params.id,
+    "face": faces[req.params.id]
+  }]};
+
+  res.send();
+});
+
+app.listen('1337');
